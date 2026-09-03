30aec0b8c0738bcca6d1f02c6d95ca46de72ad94
TAKAHASHI Shuuji <id@shuuji3.xyz>
2026-09-04 01:05:30 +0900

    fix: correct per-type truncation warning and imporove log message

--- locations/spiders/japan_post_jp.py
+++ locations/spiders/japan_post_jp.py
@@ -18,7 +18,7 @@ MAP_ID = "search"
 class JapanPostJPSpider(Spider):
     name = "japan_post_jp"
 
-    def make_request(self, lat, lon, radius, offset=1, count=900):
+    def make_request(self, lat, lon, radius, offset=1, count=900, tempo_count=0, post_count=0):
         params = {
             "cid": MAP_ID,
             "postcid": "searchPO",
@@ -54,6 +54,7 @@ class JapanPostJPSpider(Spider):
                 "lat": lat,
                 "lon": lon,
                 "offset": offset,
+                "count": count,
                 "tempo_count": tempo_count,
                 "post_count": post_count,
             },
@@ -66,7 +67,7 @@ class JapanPostJPSpider(Spider):
         for city in city_locations("JP", 200000):
             yield self.make_request(city["latitude"], city["longitude"], 5500)
 
-    def parse(self, response, lat, lon, offset):
+    def parse(self, response, lat, lon, offset, count=900, tempo_count=0, post_count=0):
         # response is an EUC-encoded JS file that looks like
         #   ZdcEmapHttpResult[1] = '...';
         # where the string body is a TSV
@@ -79,11 +80,22 @@ class JapanPostJPSpider(Spider):
         reader = csv.reader(StringIO(tsv_str), delimiter="\t")
         ret_code, rec_count, hit_count = map(int, next(reader))
         assert rec_count <= hit_count, (rec_count, hit_count)
-        if hit_count >= MAX_ITEMS:
-            self.logger.warning("Maximum number of items returned in one query, consider lowering the radius")
-        if rec_count >= hit_count:
-            yield self.make_request(lat, lon, offset + rec_count)
-        for row in reader:
+        rows = list(reader)
+        tempo_total = tempo_count + sum(1 for r in rows if r[0] == "TEMPO")
+        post_total = post_count + sum(1 for r in rows if r[0] == "POST")
+
+        if tempo_total >= MAX_ITEMS or post_total >= MAX_ITEMS:
+            f"Maximum number of items returned in one query, consider lowering the radius to avoid locations (lat={lat}, lon={lon}, tempo={tempo_total}, post={post_total})"
+        else:
+            page = (offset - 1) // count + 1
+            self.logger.info(
+                f"Query OK (lat={lat}, lon={lon}, page={page}, offset={offset}, rec={rec_count}, hit={hit_count}, tempo={tempo_total}, post={post_total})"
+            )
+
+        if offset + rec_count < hit_count:
+            yield self.make_request(lat, lon, offset + rec_count, tempo_count=tempo_total, post_count=post_total)
+
+        for row in rows:
             if row[0] == "POST":
                 item = Feature()
                 item["ref"] = row[1]
